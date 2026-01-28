d321def318815bdab55740954f71b6652618dd2b
Vivek Bhadra <vivek.bhadra@gmail.com>
2026-01-28 16:35:12 +0000

    Making modifications.

--- projects/chapter_12/condition_variable_example.cpp
+++ projects/chapter_12/condition_variable_example.cpp
@@ -1,25 +1,26 @@
 // condition_variable_example.cpp
 // This code demonstrates the use of std::condition_variable in C++17.
-// A producer thread prepares data and notifies a consumer thread
-// which waits for the data to be ready before processing it.
+// A producer thread generates data and notifies a consumer thread,
+// which waits until data becomes available before processing it.
+
 #include <condition_variable>
 #include <iostream>
 #include <mutex>
-#include <string>
 #include <thread>
+#include <vector>
 
-std::mutex mtx;             // Mutex for synchronizing access
-std::condition_variable cv; // Condition variable for notification
-bool ready = false;         // data availability flag
-std::string message;        // Shared data
+std::mutex mtx;             	// Mutex for synchronizing access
+std::condition_variable cv; 	// Condition variable for notification
+bool ready = false;         	// data availability flag
+std::vector<int> shared_data;	// Shared data
 
 void producer()
 {
     {
-        std::lock_guard<std::mutex> lock(mtx); // Lock the mutex
-        message = "Data prepared by producer"; // data populated by producer
+        std::lock_guard<std::mutex> lock(mtx); 	// Lock the mutex
+        shared_data.push_back(42); 		        // data populated by producer
         ready = true;
-        std::cout << "Producer: message prepared.\n";
+        std::cout << "Producer: data produced.\n";
     }                // Unlocks mutex here
     cv.notify_one(); // Notify one waiting thread
                      // or cv.notify_all() to wake all waiting threads
@@ -27,10 +28,12 @@ void producer()
 
 void consumer()
 {
-    std::unique_lock<std::mutex> lock(mtx); // we need more controlle over the lock
+    std::unique_lock<std::mutex> lock(mtx); // we need more control over the lock
                                             // hence std::unique_lock, not std::lock_guard
     cv.wait(lock, [] { return ready; });    // Wait until ready becomes true
-    std::cout << "Consumer: received -> " << message << '\n';
+    int value = shared_data.back();
+    shared_data.pop_back(); // Consume data
+    std::cout << "Consumer: received -> " << value << '\n';
 }
 
 int main()
